77351c6144e9e068cda6f7f2d2293ecca2a10097
JoseeeLuis <jose.aranda@gm2dev.com>
2023-08-02 10:24:51 -0300

    feat(script.js):add functionality to tab component

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let tabList =document.querySelectorAll(".tab-button");
+let tabContentList = document.querySelectorAll(".tab-content");
+
+tabList.forEach(
+
+  (tab) => {
+    tab.addEventListener("click", (event) => {
+      // remover la clase active del resto
+      tabList.forEach((tabElement) => {
+        tabElement.classList.remove("active");
+      });
+      tabContentList.forEach((contentElement) => {
+        contentElement.classList.remove("active");
+
+        if (contentElement.id === `content-${event.target.id}`) {
+          contentElement.classList.add("active");
+        }
+      });
+      
+      //agrega la clase active al clickeado
+      event.target.classList.add("active");
+    });
+  }
+);
